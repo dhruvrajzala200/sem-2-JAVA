fd62954b36ad787577fc3206e5d43c828158b9cc
dhruvrajzala200 <142960525+dhruvrajzala200@users.noreply.github.com>
2024-01-02 11:28:20 +0530

    Add files via upload

---
+++ ScannerClass.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class ScannerClass
+{
+	public static void main(String[] args)
+{
+	Scanner sc = new Scanner(System.in);
+	String a=sc.nextInt();
+	System.out.println(a);
+   }
+}
