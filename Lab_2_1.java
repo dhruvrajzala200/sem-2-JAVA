5252beac59afc9e36997a292e28a42b99a2097ed
dhruvrajzala200 <142960525+dhruvrajzala200@users.noreply.github.com>
2024-01-09 11:20:25 +0530

    Add files via upload

---
+++ Lab_2_1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public  class Lab_2_1 {
+	public static void main(String[] args){
+		int n1=Integer.parseInt(args[0]);
+		int n2=Integer.parseInt(args[1]);
+
+		System.out.println(n1+"+"+n2+"="+(n1+n2));
+
+
+		Scanner sc= new Scanner(System.in);
+		System.out.print("Enter first number = ");
+		int n3=sc.nextInt();
+		System.out.print("Enter second number = ");
+		int n4=sc.nextInt();
+
+		System.out.println(n3+"+"+n4+"="+(n3+n4));
+
+
+	}
+}
