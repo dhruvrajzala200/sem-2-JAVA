d863da1621854106d2f09ab347879e99f23e729a
dhruvrajzala200 <142960525+dhruvrajzala200@users.noreply.github.com>
2024-01-23 10:59:00 +0530

    Add files via upload

---
+++ LAB_4_5_2.java
@@ -0,0 +1,10 @@
+import java.util.*;  
+public class LAB_4_5_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string = ");
+        String str = sc.nextLine();
+        System.out.println("String Length ="+ str.length());
+      }
+    
+}
