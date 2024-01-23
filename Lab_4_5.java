3df94ad062c52c14fad63c856c1bebc6af87853a
dhruvrajzala200 <142960525+dhruvrajzala200@users.noreply.github.com>
2024-01-23 10:57:55 +0530

    Update Lab_4_5.java

--- Lab_4_5.java
+++ Lab_4_5.java
@@ -1,17 +1,18 @@
-import java.util.Scanner;
-public class Lab_4_5
-{
-	public static void main(String[] args) 
-	{
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a string : ");
-		String s = sc.next();
-		System.out.println("Length of string = "+s.length());
-		int i;
-		System.out.print("Second half of string = ");
-		for(i=(s.length())/2;i<s.length();i++)
-		{
-			System.out.print(""+s.charAt(i));
-		}
-	}
+import java.util.*;
+    public class Lab_4_5 {
+
+        public static void main(String[] args) {
+            Scanner sc= new Scanner(System.in);
+            System.out.println("Enter the string =");
+            String str=sc.nextLine();
+            int i=0;
+         for(char ch: str.toCharArray()){
+            i++;
+         }
+        System.out.println("string lenght="+i);
+
+         for(i=(str.length())/2;i<str.length();i++){
+            System.out.print(""+str.charAt(i));
+         }
+    }
 }
